ed0034c561c346593006d46323959b816f5e4597
John Canero <johncanero.official@gmail.com>
2022-08-05 22:22:12 +0800

    Input Note.js Array List and Prepare Initial Props

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,9 +1,31 @@
+import notes from '../notes'
+
+
 const Note = () => {
+
+  // Props Function
+  const Card = (props) => {
+      return (
+          <div className='note'>
+              
+          </div>
+      );
+  }
+
+
+
+  // Map Function
+
+
+
   return (
     <div className="note">
         <h1>This is the note title</h1>
         <p>This is the note content</p>
     </div>
+
+    // notes.map
+
   )
 }
 
