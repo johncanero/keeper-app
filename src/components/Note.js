c8509cf61bff2a4faa6cc940cfd6b0c2e4b46e73
John Canero <johncanero.official@gmail.com>
2022-08-11 13:01:20 +0800

    Add Packages: and Add a Delete Icon

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,3 +1,5 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+
 const Note = (props) => {
 
   //6. Implement the delete note functionality.
@@ -9,7 +11,9 @@ const Note = (props) => {
     <div className="note">
         <h1>{props.title}</h1>
         <p>{props.content}</p>
-        <button onClick={handleClick}>DELETE</button>
+        <button onClick={handleClick}>
+            <DeleteIcon />
+        </button>
   </div>
   )
 }
