fe32b68f7207e1cb04cb17399e3c3f56c4520c20
John Canero <johncanero.official@gmail.com>
2022-08-11 09:32:28 +0800

    Input Comments

--- src/components/Note.js
+++ src/components/Note.js
@@ -9,3 +9,9 @@ const Note = (props) => {
 }
 
 export default Note
+
+//CHALLENGE:
+//2. Implement the delete note functionality.
+//- Callback from the Note component to trigger a delete function.
+//- Use the filter function to filter out the item that needs deletion.
+//- Pass a id over to the Note component, pass it back to the App when deleting.
